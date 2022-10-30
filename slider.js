998a4f66276213a7a5f658ed3e44e1ca18c4747b
nnwv <29682995+nnwv@users.noreply.github.com>
2022-10-30 15:09:24 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,17 +1,20 @@
-<script>
-    var imgArray = [
-        'images/image1.jpg',
-        'images/image2.jpg',
-        'images/image3.jpg'
-    ];
-    var curIndex = 0;
-    var imgDuration = 5000;
+ <script>
+        var imgArray = [
+            'https://tile.loc.gov/storage-services/service/pnp/npcc/30000/30071r.jpg',
+            'https://tile.loc.gov/storage-services/service/pnp/gtfy/00600/00634_150px.jpg',
+            'https://tile.loc.gov/storage-services/service/pnp/fsa/8b16000/8b16100/8b16145_150px.jpg'
+            'https://tile.loc.gov/storage-services/service/pnp/habshaer/ct/ct0000/ct0003/photos/022087pr.jpg'
+            
+            
+        ];
+        var curIndex = 0;
+        var imgDuration = 10000;
 
-    function slideShow() {
-        document.getElementById('image1').src = imgArray[curIndex];
-        curIndex++;
-        if (curIndex == imgArray.length) { curIndex = 0; }
-        setTimeout("slideShow()", imgDuration);
-    }
-    slideShow();
-</script>
+        function slideShow() {
+            document.getElementById('image1').src = imgArray[curIndex];
+            curIndex++;
+            if (curIndex == imgArray.length) { curIndex = 0; }
+            setTimeout("slideShow()", imgDuration);
+        }
+        slideShow();
+    </script>
