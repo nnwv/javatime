ff25ee7d4a38526f56e891af3ef8d417a9c2b803
nnwv <29682995+nnwv@users.noreply.github.com>
2022-10-30 15:29:09 -0400

    Update slider.js

--- slider.js
+++ slider.js
@@ -3,6 +3,7 @@
             'https://tile.loc.gov/storage-services/service/pnp/npcc/30000/30071r.jpg',
             'https://tile.loc.gov/storage-services/service/pnp/gtfy/00600/00634_150px.jpg',
             'https://tile.loc.gov/storage-services/service/pnp/fsa/8b16000/8b16100/8b16145_150px.jpg'
+            'https://tile.loc.gov/storage-services/service/pnp/ppss/01000/01044v.jpg'
             
             
             
